f6e2a0f98dc24f693a4c5396026f6faab0440aea
myhql <1051576073@qq.com>
2021-10-06 20:10:16 +0800

    [add]提交121和350题

---
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,62 @@
+﻿/*
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int buy = prices[0];
+        for (int i = 0; i < prices.size(); ++i)
+        {
+            if (prices[i] > buy)
+            {
+                if (prices[i] - buy > max)
+                {
+                    max = prices[i] - buy;
+                }
+            }
+            else
+            {
+                buy = prices[i];
+            }
+           
+        }
+        return max;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    //[7,1,5,3,6,4]
+    vector<int> prices;
+   /* prices.push_back(7);
+    prices.push_back(1);
+    prices.push_back(5);
+    prices.push_back(3);
+    prices.push_back(6);
+    prices.push_back(4);*/
+
+    //[7,6,4,3,1]
+    prices.push_back(7);
+    prices.push_back(6);
+    prices.push_back(4);
+    prices.push_back(3);
+    prices.push_back(1);
+
+    Solution s;
+    printf("maxProfit %d\n", s.maxProfit(prices));
+    return 0;
+}
---
+++ 350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,93 @@
+﻿/*
+* 
+* 给定两个数组，编写一个函数来计算它们的交集。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2,2]
+示例 2:
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[4,9]
+
+说明：
+输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+我们可以不考虑输出结果的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    void print(vector<int>& res)
+    {
+        printf("[");
+        for (int i = 0; i < res.size(); ++i)
+        {
+            if (0 == i)
+                printf("%d", res[i]);
+            else
+                printf(",%d", res[i]);
+        }
+        printf("]");
+    }
+public:
+    unordered_map<int, int> make_map(vector<int>& nums)
+    {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            auto it = map.find(nums[i]);
+            if (it != map.end())
+                it->second++;
+            else
+                map.insert(make_pair(nums[i], 1));
+        }
+        return map;
+    }
+
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int,int> map1 = make_map(nums1),
+            map2 = make_map(nums2);
+        
+        for (auto& it1 : map1)
+        {
+            auto it2 = map2.find(it1.first);
+            if (it2 != map2.end())
+            {
+                int t = it1.second > it2->second ? it2->second : it1.second;
+                for (int i = 0; i < t; ++i)
+                    res.push_back(it1.first);
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    //nums1 = [1,2,2,1], nums2 = [2,2]
+    vector<int> nums1,nums2;
+    nums1.push_back(1);
+    nums1.push_back(2);
+    nums1.push_back(2);
+    nums1.push_back(1);
+
+    nums2.push_back(2);
+    nums2.push_back(2);
+
+    Solution s;
+    auto res = s.intersect(nums1, nums2);
+    s.print(res);
+    return 0;
+}
