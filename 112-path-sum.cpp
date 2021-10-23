3cc2e7ff067fffa4e037a21c8bd752a487dde3c1
myhql <1051576073@qq.com>
2021-10-23 20:01:55 +0800

    [edit]新题

---
+++ 112-path-sum.cpp
@@ -0,0 +1,105 @@
+﻿/*
+*给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，
+判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+
+叶子节点 是指没有子节点的节点。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+#include <stack>
+using namespace std;
+
+class Solution {
+    bool hasPathSumChild(TreeNode* root, int val, int targetSum) {
+        if (NULL == root->left && NULL == root->right)
+        {
+            return val == targetSum;
+        }
+        if (root->left && hasPathSumChild(root->left, val + root->left->val, targetSum))
+            return true;
+        if (root->right && hasPathSumChild(root->right, val + root->right->val, targetSum))
+            return true;
+        return false;
+    }
+public:
+    bool hasPathSum1(TreeNode* root, int targetSum) {
+        if (NULL == root)
+            return false;
+        return hasPathSumChild(root, root->val, targetSum);
+    }
+    //迭代
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (NULL == root)
+            return false;
+
+        TreeNode* cur = root,*pre=NULL;
+        stack<TreeNode*>st;
+        //st.push(root);
+        int cur_val = 0;
+        while (!st.empty() || cur)
+        {
+            if (cur)
+            {
+                st.push(cur);
+                cur_val += cur->val;
+                cur = cur->left;
+            }
+            else
+            {
+                cur = st.top();
+                if (NULL == cur->left && NULL == cur->right)
+                {
+                    //continue;
+                    if (cur_val == targetSum)
+                        return true;
+                }
+
+                if (cur->right&& cur->right !=pre)
+                {
+                    cur = cur->right;
+                    continue;
+                }
+                st.pop();
+                cur_val -= cur->val;
+                pre = cur;
+                cur = NULL;
+            }
+        }
+        return false;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    TreeNode* node7 = new TreeNode(7);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node11 = new TreeNode(11, node7, node2);
+    TreeNode* rnode4 = new TreeNode(4, node11, NULL);
+
+    TreeNode* node1 = new TreeNode(1);
+    TreeNode*  node4 = new TreeNode(4,NULL, node1);
+    TreeNode* node13 = new TreeNode(13);
+    TreeNode* node8 = new TreeNode(8, node13, node4);
+
+    TreeNode* root = new TreeNode(5, rnode4, node8);
+    Solution s;
+    s.hasPathSum(root, 22);
+    return 0;
+}
---
+++ 145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,100 @@
+﻿/*
+* 给定一个二叉树，返回它的 后序 遍历。
+
+示例:
+
+输入: [1,null,2,3]  
+   1
+    \
+     2
+    /
+   3 
+
+输出: [3,2,1]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+*/
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+ 
+
+class Solution {
+public:
+    //递归法
+    vector<int> postorderTraversal1(TreeNode* root) {
+        vector<int> res;
+        if (NULL == root)
+            return res;
+        //dest.insert(dest.end(), src.begin(), src.end());
+        vector<int> left = postorderTraversal(root->left);
+        vector<int> right = postorderTraversal(root->right);
+
+        res.insert(res.end(), left.begin(), left.end());
+        res.insert(res.end(), right.begin(), right.end());
+        res.push_back(root->val);
+        return res;
+    }
+
+    //迭代法
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (NULL == root)
+            return res;
+        
+        //标识已经遍历过的
+        bool nodemap[200];
+        memset(nodemap, false, sizeof(nodemap));
+
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while (cur || !st.empty())
+        {
+            //左入库
+            if (cur)
+            {
+                st.push(cur);
+                cur = cur->left;
+                if (cur && cur->val == -101)
+                    cur = NULL;
+            }
+            else
+            {
+                cur = st.top();
+                if (cur->right&& cur->right->val!=-101)
+                {
+                    cur = cur->right;
+                }
+                else
+                {
+                    st.pop();
+                    res.push_back(cur->val);
+                    cur->val = -101;
+                   // nodemap[cur->val + 100] = true;
+                    cur = NULL;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    //[1, null, 2, 3]
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node2 = new TreeNode(2,node3,NULL);
+    TreeNode* root = new TreeNode(1, NULL, node2);
+    Solution s;
+    auto res = s.postorderTraversal(root);
+}
